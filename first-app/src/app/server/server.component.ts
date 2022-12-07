6bb564d314f4ee16894cd7a580738fdc159a41c6
Marcos Bonetto <marcosrbonetto@gmail.com>
2022-12-07 12:22:39 -0300

    ngClass

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
 
 export class ServerComponent{
+    serverStatus = 'offline';
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+    }
+
+    getColor(){
+        return this.serverStatus === 'offline'? 'grey':'green';
+    }
 
 }
